211626ff0953b958a335c4307158a675ccbc4dea
maruisbg <tangouessognim@gmail.com>
2025-12-12 21:10:22 +0000

    mopd

---
+++ js/portfolio1.js
@@ -0,0 +1,8 @@
+let button2 = document.querySelector('#boutton2');
+button2.addEventListener('click', function(){
+});
+
+
+let button3 = document.querySelector('#boutton3');
+button2.addEventListener('click', function(){
+});
